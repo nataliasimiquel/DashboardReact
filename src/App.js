7b479cd6ab764ba69c334b81a979929892ffb0d3
Natália Simiquel <nsimiquel@gmail.com>
2022-05-27 16:23:14 -0300

    dashboard

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/Dashboard.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="Dashboard"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Natalia</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Dashboard from './Dashboard';
+import Dashboard from './components/dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <Dashboard />
   </React.StrictMode>
 );
